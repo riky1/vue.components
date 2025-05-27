994771620395c141e96d55738c8a567aef7635e9
Riky1 <cresti.ric@gmail.com>
2025-05-27 11:05:59 +0200

    feat: assets refactor + add button-primary

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import ButtonPrimary from './components/ButtonPrimary.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <ButtonPrimary>Ciaone</ButtonPrimary>  
 </template>
 
 <style scoped>
---
+++ src/components/ButtonPrimary.vue
@@ -0,0 +1,51 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  tag: {
+    type: String,
+    default: 'button', // Può essere 'button', 'a', o un router-link tag
+  },
+  // Altre props come href, to, type, disabled, ecc.
+  disabled: Boolean,
+  // Potresti volere una prop per l'icona
+  // icon: String,
+});
+
+const buttonClasses = computed(() => [
+  'btn',
+  'btn--primary',
+  // Aggiungi qui altre classi basate su props se necessario
+]);
+
+const buttonType = computed(() => {
+  if (props.tag === 'button') {
+    return props.type || 'button';
+  }
+  return null;
+});
+</script>
+
+<template>
+  <component
+    :is="tag"
+    :class="buttonClasses"
+    :type="buttonType"
+    :disabled="disabled"
+    role="button"
+    :aria-disabled="disabled ? 'true' : null"
+    v-bind="$attrs"
+  >
+    <!-- Esempio con slot per icona e testo -->
+    <span v-if="$slots.icon" class="btn__icon">
+      <slot name="icon"></slot>
+    </span>
+    <span class="btn__text">
+      <slot></slot> <!-- Contenuto di default del bottone (testo) -->
+    </span>
+  </component>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
